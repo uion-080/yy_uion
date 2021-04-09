a00da8695dc7ae11250535e6e5f773837d60a75c
yy <email>
2021-04-09 09:53:06 +0800

    test

---
+++ spring-study/src/main/java/com/yy/springstudy/Test.java
@@ -0,0 +1,10 @@
+package com.yy.springstudy;
+/**
+ * @author WL20190720
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println();
+    }
+}
